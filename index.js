39f056a83df5a02565dfb9bde2b9bd58b6e20e7a
Robert Carroll <robertcarrollsales@gmail.com>
2022-02-01 11:40:33 -0500

    commit tree 0ab23bfc2fd3c604ec4f1467e41138e5fba30da9

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
 
-// Write your code here
+const random = 1
+Math.random();
+
+const num3 = 31;
+const num4 = 9;
+const mod = num3 % num4;
+
+const max = Math.max(1,5,10,20);
